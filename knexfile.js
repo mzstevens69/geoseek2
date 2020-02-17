243f32108e59bb2e3d934bba94c6e70a6d75f615
fishmanjohn <john@emmaandjohnbyvan.com>
2020-02-17 14:34:08 -0800

    initial setup

---
+++ knexfile.js
@@ -0,0 +1,82 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      // location and name of DB
+      filename: './data/geoseek.db3'
+    },
+    migrations: {
+      // location of migrations for the DB
+      directory: './data/migrations'
+    },
+    seeds:{
+      //location of seed data for the DB
+      directory: './data/seeds'
+    },
+    pool:{
+      afterCreate: (conn, done)=>{
+        conn.run('PRAGMA foreign_keys = ON',done)
+      }
+
+    }
+  },
+  testing:{
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      // location and name of DB
+      filename: './data/geoseek.db3'
+    },
+    migrations: {
+      // location of migrations for the DB
+      directory: './data/migrations'
+    },
+    seeds:{
+      //location of seed data for the DB
+      directory: './data/seeds'
+    },
+    pool:{
+      afterCreate: (conn, done)=>{
+        conn.run('PRAGMA foreign_keys = ON',done)
+      }
+
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
