d114e162c871e4f00dc786158857375248e7572c
dantebui <senpaicaboose@gmail.com>
2020-02-18 13:37:29 -0700

    Added Gems Model/Router

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express')
 const cors = require('cors')
 const server = express()
 const helmet = require('helmet')
-
+const gemsRouter= require('./gems/gems-router')
 
 server.use(express.json())
 server.use(cors())
 server.use(helmet())
+server.use('/api/gems', gemsRouter)
 
 server.get('/',(req,res)=>{
     res.send('Server Running')
