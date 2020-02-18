72afe7194c525474e705e0aee5d62968b0742ebf
fishmanjohn <john@emmaandjohnbyvan.com>
2020-02-17 16:24:23 -0800

    login and register done in users router

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const cors = require('cors')
 const server = express()
 const helmet = require('helmet')
 
+const usersRouter = require('./users/users-router')
 
 server.use(express.json())
 server.use(cors())
 server.use(helmet())
+
+server.use('/api/users', usersRouter)
 
 server.get('/',(req,res)=>{
     res.send('Server Running')
