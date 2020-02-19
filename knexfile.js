4a8a34a5d7dc1f978013abe59b71b13436819f70
dantebui <senpaicaboose@gmail.com>
2020-02-19 16:20:22 -0700

    Heroku Deployment Test

--- knexfile.js
+++ knexfile.js
@@ -20,6 +20,20 @@ module.exports = {
     }
   },
 
+  herokuTest: {
+    client: 'pg',
+    connection: 
+      process.env.DATABASE_URL + `?ssl=true`
+    ,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './data/migrations',
+      tableName: 'knex_migrations'
+    }
+  },
 
   testing:{
     client: 'pg',
