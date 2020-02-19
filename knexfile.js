c920290895db29b90814a5a1a0ad43177b4a3b34
fishmanjohn <john@emmaandjohnbyvan.com>
2020-02-19 14:11:52 -0800

    working on staging

--- knexfile.js
+++ knexfile.js
@@ -43,10 +43,10 @@ module.exports = {
    staging: {
     client: 'pg',
     connection: {
-      database: 'postgres',
-      host: 'localhost',
-      user:     'postgres',
-      password: 'geoseek-admin'
+      database: 'd33m38q4mjfc6o',
+      host: 'ec2-54-80-184-43.compute-1.amazonaws.com',
+      user:     'dxwubnggbaufkf',
+      password: '7770b6ce3c6de9f83d6e96e8743fc4dd8029c24f73554f92e36fae0ebe04e7d0'
     },
     pool: {
       min: 2,
