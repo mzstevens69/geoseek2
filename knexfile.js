f87c67d24400fbf8fc62842290895edc500df6a6
dantebui <senpaicaboose@gmail.com>
2020-02-19 17:49:12 -0700

    knexfile changes

--- knexfile.js
+++ knexfile.js
@@ -72,21 +72,16 @@ module.exports = {
     }
  },
 
-  production: {
-    client: 'pg',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      directory: './data/migrations',
-      tableName: 'knex_migrations'
-    }
+ production: {
+  client: 'pg',
+  connection: process.env.DATABASE_URL,
+  migrations: {
+    tableName: 'dbmigrations',
+    directory: './data/migrations'
+  },
+  seeds: { 
+    directory: './data/seeds' 
   }
+}
 
 };
