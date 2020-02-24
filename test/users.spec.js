97dd0c5e46eba895c8275cc18c86b1d0ad97853e
fishmanjohn <john@emmaandjohnbyvan.com>
2020-02-24 08:35:32 -0800

    update to users

--- test/users.spec.js
+++ test/users.spec.js
@@ -3,26 +3,25 @@ const request = require('supertest')
 const db = require('../dbConfig')
 
 beforeEach(()=>{
-   return db.migrate.rollback()
-   .then(()=>{db.migrate.latest()})
-//    .then(()=>{db.seed.run()})
+    return db.migrate.rollback('users')
+    .then(()=>db.migrate.latest('users'))
 })
 
-// describe('Auth', ()=>{
-//     it('gets 201 from register', async ()=>{
-//         const res = await request(server)
-//         .post('/api/users/register')
-//         .send({username: 'pickleRick', email:'sechuansauce@rsanchez.com' , password:'jaguar'})
-//         expect(res.status).toBe(201)
-//     })
-//     it('gives response 401 to invalid user', async ()=>{
-//         const res = await request(server)
-//         .post('/api/users/login')
-//         .send({ username:'fart',password:'goodbyemoonmen' })
-//         expect(res.status).toBe(401)
-//     })
+describe('Auth', ()=>{
+    it('gets 201 from register', async ()=>{
+        const res = await request(server)
+        .post('/api/users/register')
+        .send({username: 'picklerick', email:'sechuansauce@rsanchez.com' , password:'jaguar'})
+        expect(res.status).toBe(201)
+    })
+    it('gives response 401 to invalid user', async ()=>{
+        const res = await request(server)
+        .post('/api/users/login')
+        .send({ username:'fart',password:'goodbyemoonmen' })
+        expect(res.status).toBe(401)
+    })
 
-// })
+})
 describe('login user',()=>{
     it('Post /api/auth/login', async ()=>{
         const register = await request(server)
