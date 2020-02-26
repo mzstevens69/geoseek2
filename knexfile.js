862fe2dc992876bb1ae3b5d7e98dc6b7fe684de5
Joshua Sharp <sharp.josh@gmail.com>
2020-02-26 10:54:40 -0500

    changed connection property

--- knexfile.js
+++ knexfile.js
@@ -48,9 +48,7 @@ module.exports = {
 
   staging: {
     client: "pg",
-    connection: {
-      connection: process.env.DATABASE_URL,
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10,
